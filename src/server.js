66ffa461fd5cc191aab269059c52c1dec7aa4fa7
Shubham Mirashi <136989957+Shubham1905s@users.noreply.github.com>
2025-07-12 01:17:38 +0530

    cron.js

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ import { eq, and } from "drizzle-orm"; // ← Make sure this is imported
 import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js"; // Assuming favoritesTable is exported from schema.js
+import job from "./config/cron.js";
 
 const app = express();
-
 const PORT = ENV.PORT || 5001;
+if(ENV.NODE_ENV === "production") job.start();
+
+
 app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true });
